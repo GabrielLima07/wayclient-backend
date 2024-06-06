2052d2c9c9869b881f6ab7bc663c6213af72578f
Gabriel Lima <102686294+GabrielLima07@users.noreply.github.com>
2024-06-06 20:29:57 -0300

    fix: try to resolve deploy errors

--- src/main/java/com/pi4/wayclient/service/TokenService.java
+++ src/main/java/com/pi4/wayclient/service/TokenService.java
@@ -14,7 +14,7 @@ import java.time.ZoneOffset;
 
 @Service
 public class TokenService {
-    @Value("${api.token.secret}")
+    @Value("${JWT_SECRET:my-secret-key}")
     private String secret;
 
     public String generateToken(User user) {
