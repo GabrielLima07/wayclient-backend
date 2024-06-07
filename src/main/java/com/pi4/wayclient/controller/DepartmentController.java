ff7799045853f4bdbb7c85bae166a55877893e2b
Filipe da Silva Granja <filipe.00000847940@unicap.br>
2024-06-07 11:30:37 -0300

    finalizando department

--- src/main/java/com/pi4/wayclient/controller/DepartmentController.java
+++ src/main/java/com/pi4/wayclient/controller/DepartmentController.java
@@ -1,2 +1,47 @@
-package com.pi4.wayclient.controller;public class DepartmentController {
+package com.pi4.wayclient.controller;
+
+import com.pi4.wayclient.model.Department;
+import com.pi4.wayclient.service.DepartmentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("department")
+public class DepartmentController {
+    private final DepartmentService departmentService;
+
+    @Autowired
+    public DepartmentController(DepartmentService departmentService){
+        this.departmentService = departmentService;
+    }
+    @PostMapping
+    public Department postDepartament(@RequestBody Department department){
+        return departmentService.createDepartment(department);
+    }
+    @GetMapping
+    public List<Department> getDepartment(){return  departmentService.getAllDepartment();}
+
+    @GetMapping("/{id}")
+    public Optional<Department> getDepartment(@PathVariable UUID id,@RequestBody Department department){
+        department.setId(id);
+        return departmentService.getDepartment(id);
+    }
+    @PutMapping("/{id}")
+    public Department putDepartment(@PathVariable UUID id, @RequestBody Department department){
+        department.setId(id);
+        return departmentService.updateDepartment(department);
+    }
+    @DeleteMapping("/{id}")
+    public String deleteDepartment(@PathVariable UUID id){
+        try{
+            departmentService.deleteDepartment(id);
+            return "Department with ID " + id + " deleted successfully";
+        }catch (Exception e){
+            return "Failed to delete department with ID" + id + ": " + e.getMessage();
+        }
+    }
 }
--- src/main/java/com/pi4/wayclient/repository/DepartmentRepository.java
+++ src/main/java/com/pi4/wayclient/repository/DepartmentRepository.java
@@ -1,2 +1,6 @@
-package com.pi4.wayclient.repository;public interface DepartmentRepository {
+package com.pi4.wayclient.repository;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.UUID;
+import com.pi4.wayclient.model.Department;
+public interface DepartmentRepository extends JpaRepository<Department, UUID> {
 }
